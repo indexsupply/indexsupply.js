1f090f15511f5c32b0e8fbfeb6ea9fed5f054038
ryan smith <r@32k.io>
2024-12-09 12:22:54 -0800

    use new static s3 bucket

--- upload-browser-index.ts
+++ upload-browser-index.ts
@@ -1,7 +1,7 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({region: "us-west-2"});
 await s3Client.send(new PutObjectCommand({
-	Bucket: "bin.indexsupply.net",
+	Bucket: "static.indexsupply.net",
 	Key: "indexsupply.js",
 	Body: await Bun.file("dist/browser-index.js").bytes(),
 }));
