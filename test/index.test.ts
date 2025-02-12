848209b432568137bec01418a06bafd229b6edd9
ryan smith <r@32k.io>
2025-02-12 15:40:47 -0800

    first pass at daimo pay integration

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
 import test from "node:test";
 import assert from 'node:assert/strict';
-import { guessBlockTime, query, queryLive } from "../src/index"
+import { Daimo, guessBlockTime, query, queryLive } from "../src/index"
 
 test("query", async (t) => {
   await t.test("should work", async () => {
@@ -77,5 +77,44 @@ test("queryLive", async (t) => {
 test("blockTimestamp", (t) => {
   t.test("valid chain", () => {
     assert.equal(guessBlockTime(8453, 26041515).toISOString(), "2025-02-06T20:06:17.000Z")
+  });
+});
+
+test("daimoPay", async (t) => {
+  await t.test("query", async () => {
+    const { blockNumber, result } = await Daimo.query({
+      chainId: 8453n,
+      destinationAddress: "0x7531f00dbc616b3466990e615bf01eff507c88d4",
+      id: "7iNRoKprdvcNbafqixKSGJrW1T8vjyHzxfRj8jmfEeSR",
+    });
+    assert.deepStrictEqual(result, [{
+      block_num: 26227261,
+      tx_hash: '0xa96bc68abf96458413392aceecdab17d85aef02520e3a4502b488c6732da4958',
+      success: true
+    }]);
+  });
+
+  await t.test("live query", async () => {
+    const controller = new AbortController();
+    const query = Daimo.queryLive({
+      abortSignal: controller.signal,
+      startBlock: async () => 26227260n,
+      chainId: 8453n,
+      query: `
+        select block_num, tx_hash, success
+        from intentfinished
+        where destinationAddr = 0x7531f00dbc616b3466990e615bf01eff507c88d4
+        order by block_num asc
+        limit 1
+      `
+    });
+    for await (const { result } of query) {
+      assert.deepStrictEqual(result, [{
+        block_num: 26227261,
+        tx_hash: '0xa96bc68abf96458413392aceecdab17d85aef02520e3a4502b488c6732da4958',
+        success: true
+      }]);
+      controller.abort();
+    }
   });
 });
